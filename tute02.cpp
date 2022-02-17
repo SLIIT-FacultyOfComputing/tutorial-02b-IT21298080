2ec5a1f30390f633e94b4e35613fb6f33681076b
Gurusinghe <99312324+IT21298080@users.noreply.github.com>
2022-02-17 11:54:14 +0000

    I have completed  tute02

--- tute02.cpp
+++ tute02.cpp
@@ -12,33 +12,51 @@ Type = 3
 OtRate = 1700
 Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
 
-#include <stdio.h>
-int main()
-{
+#include<iostream>
+using namespace std;
+
+int main(){
+   //Creating Variables 
    double salary, netSalary;
    int etype, otHrs, otRate;
-   printf("Enter Employee Type : ");
-   scanf("%d", &etype);
-   printf("Enter Salary  : ");
-   scanf("%f", &salary);
-   printf("Enter OtHrs : ");
-   scanf("%d", &otHrs); 
-   
-   switch (etype) {
-      case 1 :
-          otRate = 1000;
-          break;
-      case 2 :
-          otRate = 1500;
-          break;
-      default :
-          otRate = 1700;
-          break;
-   }
 
+   //Get user inputs
+   cout<<"Enter Employee Type : ";
+   cin>> etype;
 
-   netSalary = salary + otHrs* otRate;
-   printf("Net Salary is %f ", netSalary);
+  cout << endl;
   
-   return 0;
-}
+   cout<<"Enter Salary : ";
+   cin >> salary;
+
+  cout << endl;
+
+   cout<<"Enter OtHrs : ";
+   cin >> otHrs;
+
+  cout << endl;
+  
+   //Conditions
+   switch (etype) {
+       case 1 : 
+            otRate = 1000;
+            break;
+       case 2 :
+            otRate = 1500;
+            break;
+       default :
+            otRate = 1700;
+            break;     
+ }
+   //Calculation
+   netSalary = salary + otHrs* otRate;
+   
+   //Printing Outputs
+   cout<<"Net Salary is " << netSalary <<endl;
+
+
+  cout << endl;
+  
+  
+   return 0;   
+  }
