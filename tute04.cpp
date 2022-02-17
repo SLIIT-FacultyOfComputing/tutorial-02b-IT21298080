2210a6bb6ab3d74ec2af9469b5cf594eaf88e297
Gurusinghe <99312324+IT21298080@users.noreply.github.com>
2022-02-17 12:16:55 +0000

    I have completed  tute04

--- tute04.cpp
+++ tute04.cpp
@@ -11,19 +11,46 @@ long Factorial(int no);
 long nCr(int n, int r);
 Do not modify the main function.*/
 
-#include <iostream>
+#include<iostream>
 
+//define function
 long Factorial(int no);
-long nCr(int n, int r);
+long nCr(int n , int r);
 
-int main() {
-  int n, r;
-  std::cout << "Enter a value for n ";
+int main()
+{
+int n , r;
+  
+  std::cout << "Enter a value for n : ";     
   std::cin >> n;
-  std::cout << "Enter a value for r ";
+
+  std::cout << "Enter a value for r : ";   
   std::cin >> r;
+
   std::cout << "nCr = ";
   std::cout << nCr(n,r);
   std::cout << std::endl;
-  return 0;
+ 
+}
+
+//implement functins
+long Factorial(int no)
+{
+  int r;
+  long fac = 1;
+
+  //Creating for loop
+  for(r = no ; r >= 1 ; r--)
+    {
+      fac = fac * r;
+    }
+  
+  return fac; 
+}
+
+long nCr(int n , int r)
+{
+  long nCr = Factorial(n) / (Factorial(r) * Factorial(n-r));
+
+  return nCr;
 }
